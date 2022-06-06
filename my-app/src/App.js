2679cc56379a363bed95c5135c848046f5f1a9e5
batrakovd2 <batrakovd2@gmail.com>
2022-06-06 13:01:05 +0500

    useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [likes, setLikes] = useState(0); 
+ 
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function dencrement() {
+    setLikes(likes - 1);
+  }
+  
   return (
     <div className="App">
-        <button>Increment</button>
-        <button>Decrement</button>
+        <h1>{likes}</h1>    
+        <button onClick={increment}>Increment</button>
+        <button onClick={dencrement}>Decrement</button>
     </div>
   );
 }
