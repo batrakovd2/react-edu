073f88c9226b008d50967dc8bb5ac01dbe09134f
batrakovd2 <batrakovd2@gmail.com>
2022-06-13 23:18:39 +0500

    edit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/app.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/modal/MyModal';
+import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
+import { useFetching } from './hooks/useFetching';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
 
 function App() {
   
-  const [posts, setPosts] = useState([
-    {id: 11, title:'JS 1', body: 'Descr'},
-    {id: 22, title:'JS 2', body: 'Descr'},
-    {id: 33, title:'JS 3', body: 'Descr'}
-  ]);
-  const [selectedSort, setSelectedSort] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const [filter, setFilter] = useState({sort: '', query: ''});
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
+ 
+  useEffect(() => {
+    fetchPosts();
+  }, [])
 
   const createPost = (newPost) => {
-    setPosts([...posts, newPost])
+    setPosts([...posts, newPost]);
+    setModal(false);
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    //setPost([...posts].sort((a,)))
-
-  }
-
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      
       <hr style={{margin: '15px 0'}}/>
-      <div>
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue={"Сортировка по"}
-          options={[
-            {value: 'title', name: 'По названию'},
-            {value: 'body', name: 'По описанию'}
-          ]}
-        />
-      </div>
-
-      {posts.length !== 0 
-        ? <PostList remove={removePost} posts={posts} title="Список постов 1"/>
-        : <h1 style={{textAlign: 'center'}}> Посты не найдены </h1>
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1> Произошла ошибка ${postError}</h1>}
+      {isPostLoading 
+        ? <Loader />
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1"/>
       }
       
-  
+
     </div>
   );
 }
