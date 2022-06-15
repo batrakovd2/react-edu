8ab13e5fba1ace383a3b10478bd0a64af215c475
batrakovd2 <batrakovd2@gmail.com>
2022-06-15 08:54:52 +0500

    edit

--- my-app/src/pages/Post.jsx
+++ my-app/src/pages/Post.jsx
@@ -8,12 +8,19 @@ const Post = () => {
     const params = useParams();
     //console.log(params);
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching(async () => {
         const response = await PostService.getPostById(params.id);
         setPost(response.data);
     });
+    const [fetchComments, isCommentLoading, errorComment] = useFetching(async () => {
+        const response = await PostService.getPostCommentsById(params.id);
+        setComments(response.data);
+    });
+
     useEffect(() => {
-        fetchPostById()
+        fetchPostById();
+        fetchComments();
     }, []);
     return(
         <div>
@@ -23,8 +30,22 @@ const Post = () => {
                     ? <Loader />
                     : <div>{post.id}  {post.title}</div>
                 }
-                
             </h1>
+            <h1>
+                Комментарии
+            </h1>
+            {isCommentLoading
+                ? <Loader />
+                : <div>
+                    {comments.map(comm => 
+                        <div style={{marginTop: 15}}>
+                            <h3>{comm.email}</h3>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
+
+            }
         </div>
     );
 }
